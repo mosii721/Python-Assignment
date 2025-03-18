7d898406eebec5da9da0572bf98db252db2af3eb
mosii721 <mikealwanga97@gmail.com>
2025-03-18 14:12:29 +0300

    'update'

---
+++ list.py
@@ -0,0 +1,36 @@
+# Create an empty list
+my_list = []
+
+# Append elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# print(my_list)
+
+# Insert value 15
+my_list.insert(1,15)
+
+# print(my_list)
+
+# Extend my_list
+my_list.extend([50,60,70])
+
+# print(my_list)
+
+# Remove last element
+my_list.pop()
+
+# print(my_list)
+
+# Sort in ascending order
+my_list.sort()
+
+#print(my_list)
+
+# index of the value 30
+index_30 = my_list.index(30)
+
+print(index_30)
+
